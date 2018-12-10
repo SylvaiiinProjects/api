2751dea1242cade48972d60261bcd5a5ba2dc27f
Sylvaiiin <44996175+SylvaiiinProjects@users.noreply.github.com>
2018-12-10 14:01:52 +0100

    Add files via upload

--- api_code.py
+++ api_code.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+import os
+import json
+import requests
+
+headers = {
+  'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
+  'content-type': "application/json"}
+response = requests.get(os.environ['FARMWARE_URL'] + '/api/v1/bot/state',
+              headers=headers)
+
+bot_state = response.json()
+position_x = bot_state['location_data']['position']['x']
+pin_13_value = bot_state['pins']['13']['value']
+
+
+"""
 'Get specific data (such as timezone) from the FarmBot Web App.'
 
 import os
@@ -16,4 +32,4 @@ device_data = response.json()
 timezone_string = device_data['timezone']
 tz_offset_hours = device_data['tz_offset_hrs']
 
-
+"""
