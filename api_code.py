2f3992d7327b10646181fbfc5ce4ee48ab23c455
Sylvaiiin <44996175+SylvaiiinProjects@users.noreply.github.com>
2018-12-10 13:34:22 +0100

    Add files via upload

--- api_code.py
+++ api_code.py
@@ -1,40 +1,34 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
 
-"""Save sensor value."""
+#The API is used to access the database (plants, sequences, points, etc.).
+
+## GET
+
+# Exemple 1
 
 import os
-import sys
 import json
-from time import time
 import requests
-
-FARMWARE_NAME = 'API'
-HEADERS = {
-    'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
-    'content-type': 'application/json'}
-
-"""def get_input_env():
-    prefix = FARMWARE_NAME        
-    input_title = os.environ.get(prefix+"_pin")
-    return input_title"""
+#from farmware_tools import app
+#points = app.get('points')
 
 
-""" 64 is always taken """
 
+headers = {'Authorization': 'Bearer ' + os.environ['API_TOKEN'],
+           'content-type': "application/json"}
 
-def no_data():
+response = requests.get('https://my.farmbot.io/api/points', headers=headers)
+points = response.json()['args']['location']['args']['x']
+
+def post(wrapped_data):
+    """Send the Celery Script command."""
+    payload = json.dumps(wrapped_data)
+    requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
+                  data=payload, headers=headers)
+
+def log(value):
     
-    message = '[Soil sensor Value] Pin {} value is not available.'.format(PIN)
-    wrapped_message = {
-        'kind': 'send_message',
-        'args': {
-            'message_type': 'error',
-            'message': message}}
-    post(wrapped_message)
-
-def data(value):
-    
-    message = '[Soil sensor Value] Pin {} value  is {}.'.format(PIN,value)
+    message = '[] point  is {}.'.format(value)
     wrapped_message = {
         'kind': 'send_message',
         'args': {
@@ -42,34 +36,25 @@ def data(value):
             'message': message}}
     post(wrapped_message)
 
-def get_pin_value():
-    """ Sequence `Read Pin` """
-    response = requests.get(
-        os.environ['FARMWARE_URL'] + 'api/v1/bot/state',
-        headers=HEADERS)
-    try:
-        value = response.json()['pins']['64']['value']
-    except KeyError:
-        value = None
-    if value is None:
-        no_data()
-        sys.exit(0)
-    else:
-        data(value)
-        sys.exit(0)
-    return value
+if __name__=='__main__':
+	log(5)
+	#p=points(['x'])
+	log(points)
 
+"""
+# Exemple 2
+'Get specific data (such as timezone) from the FarmBot Web App.'
 
+import os
+import requests
 
+headers = {'Authorization': 'Bearer ' + os.environ['API_TOKEN'],
+           'content-type': "application/json"}
+response = requests.get('https://my.farmbot.io/api/device', headers=headers)
+device_data = response.json()
 
-def post(wrapped_data):
-    """Send the Celery Script command."""
-    payload = json.dumps(wrapped_data)
-    requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
-                  data=payload, headers=HEADERS)
+# Device timezone info (set via the dropdown in the Web App Device widget)
+timezone_string = device_data['timezone']
+tz_offset_hours = device_data['tz_offset_hrs']
 
-if __name__ == '__main__':
-    PIN = 64   
-   
-    get_pin_value()
-    
+print('My device timezone is: {} (UTC{})'.format(timezone_string, tz_offset_hours)) """
