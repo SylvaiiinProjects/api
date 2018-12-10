2b93fa6bd5db4ea9de71fcd3a92cc122df3bb4a8
Sylvaiiin <44996175+SylvaiiinProjects@users.noreply.github.com>
2018-12-10 11:46:31 +0100

    Add files via upload

--- api_code.py
+++ api_code.py
@@ -52,8 +52,6 @@ def post(wrapped_data):
     requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
                   data=payload, headers=HEADERS)
 
-if __name__ == '__main__':
-       data(5)
-       sys.exit(0) 
+if __name__ == '__main__':       
        get_point()
     
