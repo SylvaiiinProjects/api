a371db58a34f5f93cb44bfef23a997790a86d708
Sylvaiiin <44996175+SylvaiiinProjects@users.noreply.github.com>
2018-12-10 16:09:20 +0100

    Add files via upload

--- api_code.py
+++ api_code.py
@@ -1,31 +1,78 @@
-#!/usr/bin/env python
+#! /usr/bin/env python
+
+#The API is used to access the database (plants, sequences, points, etc.).
+
+## GET
+
+# Exemple 1
 
 import os
+import sys
 import json
 import requests
 
-headers = {
-  'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
-  'content-type': "application/json"}
 
-def get_data():
-	response = requests.get(os.environ['FARMWARE_URL'] + 'api/v1/bot/state',
-              headers=headers)
-	try:
-	    bot_state = response.json()
-            pin_64_value = bot_state['pins']['64']['value']
-        except KeyError:
-	    pin_64_value = None
-        return pin_64_value
-
-
-if __name__=='__main__':
-	get_data()
-"""
 'Get specific data (such as timezone) from the FarmBot Web App.'
 
 import os
-import json
+import requests
+
+headers = {'Authorization': 'Bearer ' + os.environ['API_TOKEN'],
+           'content-type': "application/json"}
+
+
+
+# Device timezone info (set via the dropdown in the Web App Device widget)
+
+
+
+def data(value):
+    
+    message = '[Soil sensor Value] Hour  is {}.'.format(tz)
+    wrapped_message = {
+        'kind': 'send_message',
+        'args': {
+            'message_type': 'info',
+            'message': message}}
+    post(wrapped_message)
+
+def get_pin_value():
+    """ Sequence `Read Pin` """
+    response = requests.get('https://my.farmbot.io/api/device', headers=headers)
+    try:
+        device_data = response.json()
+        tz = device_data['tz_offset_hrs']
+    except KeyError:
+        tz = None
+    if tz is None:
+        data(tz)
+        sys.exit(0)
+    else:
+        data(tz)
+        sys.exit(0)
+    return tz
+
+
+
+def post(wrapped_data):
+    """Send the Celery Script command."""
+    payload = json.dumps(wrapped_data)
+    requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
+                  data=payload, headers=HEADERS)
+
+if __name__ == '__main__':    
+       get_pin_value()
+
+
+
+
+	
+
+"""
+# Exemple 2
+'Get specific data (such as timezone) from the FarmBot Web App.'
+
+import os
 import requests
 
 headers = {'Authorization': 'Bearer ' + os.environ['API_TOKEN'],
@@ -34,8 +81,7 @@ response = requests.get('https://my.farmbot.io/api/device', headers=headers)
 device_data = response.json()
 
 # Device timezone info (set via the dropdown in the Web App Device widget)
-
 timezone_string = device_data['timezone']
 tz_offset_hours = device_data['tz_offset_hrs']
 
-"""
+print('My device timezone is: {} (UTC{})'.format(timezone_string, tz_offset_hours)) """
