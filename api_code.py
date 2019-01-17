6f0c8ab4edb224d884bb0cbfd42594ca2b9247a8
Sylvaiiin <44996175+SylvaiiinProjects@users.noreply.github.com>
2019-01-17 10:05:22 +0100

    Update api_code.py

--- api_code.py
+++ api_code.py
@@ -1,48 +1,26 @@
-#! /usr/bin/env python
-
-#The API is used to access the database (plants, sequences, points, etc.).
-
-
-'Get specific data (such as timezone) from the FarmBot Web App.'
+#!/usr/bin/env python
 
 import os
-import json
 import requests
+import json
 
+headers = {
+  'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
+  'content-type': "application/json"}
+
+FARMWARE_NAME = 'API'
 HEADERS = {
     'Authorization': 'bearer {}'.format(os.environ['FARMWARE_TOKEN']),
-    'content-type': 'application/json'}
-
-headers = {'Authorization': 'Bearer ' + os.environ['API_TOKEN'],
-           'content-type': "application/json"}
-
-#get device info
-#response = requests.get(os.environ['FARMWARE_URL']+ 'api/device', headers=headers)
-#device_data = response.json()
-
-#get points
-res = requests.get('https://my.farmbot.io/api/points', headers=headers)
-points = res.json()
+'content-type': 'application/json'}
 
 
-"""récupère les coordonnées x,y,z des points de la web app dans une liste"""
-"""
-coord=[]
-for i in range(len(points)):
-	coord.append(points[i]['x'])
-	coord.append(points[i]['y'])
-	coord.append(points[i]['z'])"""
-	
+response = requests.get(os.environ['FARMWARE_URL'] + 'api/points',
+              headers=headers)
 
-#sortedpoints = sorted(points, key=lambda elem: (int(elem['x']), int(elem['y'])))
 
-# Device timezone info (set via the dropdown in the Web App Device widget)
-#timezone_string = device_data['timezone']
-#tz_offset_hours = device_data['tz_offset_hrs']
-	
-def data(value):
+def no_data(value):
     
-    message = '[time] Time  is {}.'.format(value)
+    message = '[Points] Points are {}.'.format(value)
     wrapped_message = {
         'kind': 'send_message',
         'args': {
@@ -51,10 +29,14 @@ def data(value):
     post(wrapped_message)
 
 def post(wrapped_data):
-    """Send the Celery Script command."""
-    payload = json.dumps(wrapped_data)
-    requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script', data=payload, headers=HEADERS)
+    
+    payload = json.dumps(wrapped_data) # convert string as object
+    requests.post(os.environ['FARMWARE_URL'] + 'api/v1/celery_script',
+   data=payload, headers=HEADERS)
 
-if __name__=='__main__':
-	data(coord)
-	#data(tz_offset_hours)
+
+bot_state = response.json()
+#posx = bot_state['location_data']['position']['x']
+
+no_data(value)
+
